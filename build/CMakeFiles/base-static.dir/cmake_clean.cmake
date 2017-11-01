cd336291f2bfd47e324dbca5473cba670b0b7ead
zxqllj <zxqllj@github.com>
2017-11-01 23:36:20 +0800

    first-commit

---
+++ build/CMakeFiles/base-static.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/base-static.dir/test.cpp.o"
+  "libbase-static.pdb"
+  "libbase-static.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/base-static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
